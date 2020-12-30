f04e83b6a88b8738a47586b5c82b6d015aa8067e
brunopirizoliveira <brunopirizoliveira@gmail.com>
2020-12-30 12:45:07 -0300

    Add Acesso a API e Dialog Authentication

--- lib/screens/transferencia/formulario.dart
+++ lib/screens/transferencia/formulario.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:byteBank/components/transaction_auth_dialog.dart';
+import 'package:byteBank/screens/transferencia/lista.dart';
 import 'package:flutter/material.dart';
 import 'package:byteBank/models/transferencia.dart';
 import 'package:byteBank/components/editor.dart';
+import 'package:http/http.dart';
 
 const _tituloAppBar = 'Criando Transferência';
 const _textoValorBotaoConfirmar = 'Confirmar';
@@ -16,12 +21,36 @@ class FormularioTransferencia extends StatefulWidget {
   void _criaTransferencia(BuildContext context) {
     final int numeroConta = int.tryParse(_controladorNumeroConta.text);
     final double valor = double.tryParse(_controladorValor.text);
+    showDialog(
+        context: context,
+        builder: (context) {
+          return TransactionAuthDialog(
+            onConfirm: (String password) {
+              if (password == '1111') {
+                if (numeroConta != null && valor != null) {
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  this.saveTransaction(transferenciaCriada);
+                }
+              }
+            },
+          );
+        }).then((value) {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => ListaTransferencias()));
+    });
+  }
 
-    if (numeroConta != null && valor != null) {
-      final transferenciaCriada = Transferencia(valor, numeroConta);
-      Navigator.pop(context, transferenciaCriada);
-      debugPrint('$transferenciaCriada');
-    }
+  Future<Response> saveTransaction(Transferencia transferencia) async {
+    final Response response = await post('http://192.168.0.111:8000/transaction',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'conta': transferencia.numeroConta,
+          'valor': transferencia.valor,
+        }));
+    debugPrint(response.body);
+    return response;
   }
 
   @override
@@ -52,6 +81,7 @@ class FormularioTransferenciaState extends State<FormularioTransferencia> {
               icon: Icons.monetization_on,
             ),
             RaisedButton(
+              // onPressed: () => widget._approveTransaction(context),
               onPressed: () => widget._criaTransferencia(context),
               child: Text(_textoValorBotaoConfirmar),
             ),
--- lib/screens/transferencia/lista.dart
+++ lib/screens/transferencia/lista.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:byteBank/components/progress.dart';
 import 'package:flutter/material.dart';
 import 'package:byteBank/models/transferencia.dart';
 import 'package:byteBank/screens/transferencia/formulario.dart';
+import 'package:http/http.dart';
 
 const _tituloAppBar = 'Transferências';
 
@@ -20,36 +24,90 @@ class ListaTransferenciasState extends State<ListaTransferencias> {
       appBar: AppBar(
         title: Text(_tituloAppBar),
       ),
-      body: ListView.builder(
-        itemCount: widget._transferencias.length,
-        itemBuilder: (context, indice) {
-          final transferencia = widget._transferencias[indice];
-          return ItemTransferencia(transferencia);
-        },
-      ),
+      body: FutureBuilder<List<Transferencia>>(
+          future:
+              Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+          // future: findAll(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                Progress(
+                  message: "Carregando Parceiros",
+                );
+                break;
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                if (snapshot.hasData) {
+                  final List<Transferencia> TransferenciaList = snapshot.data;
+                  debugPrint(TransferenciaList.toString());
+                  if (TransferenciaList.isNotEmpty) {
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: TransferenciaList.length,
+                      itemBuilder: (context, index) {
+                        final Transferencia transf = TransferenciaList[index];
+                        return ItemTransferencia(transf);
+                      },
+                    );
+                  }
+                }
+
+                return CenteredMessage(
+                  message: "Nenhum parceiro encontrado",
+                  iconData: Icons.warning,
+                );
+
+                break;
+            }
+            return CenteredMessage(
+              message: "Unknown error",
+              iconData: Icons.error,
+            );
+          }),
+
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           final Future<Transferencia> future =
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
             return FormularioTransferencia();
           }));
-          future.then((transferenciaRecebida) {
-            atualizaLista(transferenciaRecebida);
+          future.then((value) {
+            findAll();
           });
         },
       ),
     );
   }
 
-  void atualizaLista(Transferencia transferenciaRecebida) {
-    Future.delayed(Duration(seconds: 1), () {
-      if (transferenciaRecebida != null) {
-        setState(() {
-          widget._transferencias.add(transferenciaRecebida);
-        });
-      }
-    });
+  // void atualizaLista(Transferencia transferenciaRecebida) {
+  //   debugPrint('$transferenciaRecebida');
+  //   Future.delayed(Duration(seconds: 1), () {
+  //     if (transferenciaRecebida != null) {
+  //       setState(() {
+  //         widget._transferencias.add(transferenciaRecebida);
+  //       });
+  //     }
+  //   });
+  // }
+
+  Future<List<Transferencia>> findAll() async {
+    final Response response = await get('http://192.168.0.111:8000/transaction')
+        .timeout(Duration(seconds: 15));
+
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    final List<Transferencia> list = new List();
+
+    for (Map<String, dynamic> element in decodedJson) {
+      Transferencia t = Transferencia(double.parse(element['valor']), int.parse(element['conta']));
+      list.add(t);
+    }
+    debugPrint(list.toString());
+    return list;
   }
 }
 
@@ -66,6 +124,31 @@ class ItemTransferencia extends StatelessWidget {
         title: Text(_transferencia.valor.toString()),
         subtitle: Text(_transferencia.numeroConta.toString()),
       ),
+    );
+  }
+}
+
+class CenteredMessage extends StatelessWidget {
+  final String message;
+  final IconData iconData;
+
+  CenteredMessage({this.message, this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      child: Column(
+        children: <Widget>[
+          Icon(iconData),
+          Text(
+            message,
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+        ],
+      ),
+      visible: iconData != null,
     );
   }
 }
